5a626a28f14c981f22a259e4820578069a883d64
dev.paramjot <50148441+ParamjotSingh5@users.noreply.github.com>
2023-09-02 02:06:06 +0530

    Update api.js
    removed API key

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ export const geoApiUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo/';
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'b2ca028964msh36d7fdf67729852p1fa90ajsnc6f194863235',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
