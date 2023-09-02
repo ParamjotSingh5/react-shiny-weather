6f781608fb005b1534e4c31fdd28e5eb1b5ae6b1
Paramjot Singh <paramjotdhiman5@gmail.com>
2023-09-02 14:12:48 +0530

    feat: current weather componenets

--- src/components/currentWeather/current-weather.js
+++ src/components/currentWeather/current-weather.js
@@ -4,10 +4,36 @@ const CurrentWeather = () =>{
     return (
         <div className="weather">
             <div className="top">
-                <p className="city">Belgrade</p>
-                <p className="weather-description">Sunny</p>
-            </div>
-            <img alt="weather" className="weather-icon" src="icons/01d.png" />
+                <div>
+                    <p className="city">Belgrade</p>
+                    <p className="weather-description">Sunny</p>
+                </div>                
+                <img alt="weather" className="weather-icon" src="icons/01d.png" />
+            </div>           
+            <div className="bottom">
+                <p className="temperature">18°C</p>
+                <div className="details">
+                    <div className="parameter-row">
+                        <span className="parameter-label top">Details</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Feel like</span>
+                        <span className="parameter-value">22°C</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Wind</span>
+                        <span className="parameter-value">2 m/s</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Humidity</span>
+                        <span className="parameter-value">15%</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Pressure</span>
+                        <span className="parameter-value">15 hpa</span>
+                    </div>
+                </div>
+            </div> 
         </div>
     );
 }
